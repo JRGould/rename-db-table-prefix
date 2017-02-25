05ea6a17a148c7a4329577e7fa1e772b0370bf60
Jeff Gould <jrgould@gmail.com>
2017-02-24 16:25:20 -0800

    cleanup

--- wp-change-table-prefix.php
+++ wp-change-table-prefix.php
@@ -8,7 +8,7 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link              http://jrgould.com
+ * @link              http://jrgould.com/wpctp/
  * @since             1.0.0
  * @package           WPCTP
  *
